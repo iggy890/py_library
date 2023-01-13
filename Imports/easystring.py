16dab0d406097b15eb5bf75b45f4d2d8d3a0c4e3
iggy890 <98705626+iggy890@users.noreply.github.com>
2023-01-13 21:27:24 +0000

    Add easystring

---
+++ Imports/easystring.py
@@ -0,0 +1,10 @@
+def get_string_matches(string: str, string2: str):
+    matches = 0
+    string, string2 = string.lower(), string2.lower()
+
+    for i in string:
+        for j in string2:
+            if i == j:
+                matches += 1
+
+    return matches
